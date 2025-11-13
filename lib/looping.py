3fc03a7503da0a55ecb6271b634a8bfc7f9a8250
Christine Mumbi <christinemumbi286@gmail.com>
2025-11-13 14:48:10 +0300

    Adds the loops in each function

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!") 
     # code goes here!
     pass
 
 def square_integers(int_list):
+    square_list = []
+    for i in int_list:
+        square_list.append(i ** 2)
+    return square_list
     # code goes here!
     pass
 
 def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     # code goes here!
     pass
